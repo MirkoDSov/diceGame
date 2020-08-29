5e99c25a325e05f1fc3f798fbd9087d4f1b7363e
MirkoDSov <70020530+MirkoDSov@users.noreply.github.com>
2020-08-29 07:00:39 -0700

    set1

---
+++ index.js
@@ -0,0 +1,14 @@
+var countOfImages = 6;
+var num1 = Math.floor(Math.random() * countOfImages) +1;
+var num2 = Math.floor(Math.random() * countOfImages) +1;
+
+document.querySelector(".img1").setAttribute("src", "images/dice" + num1 + ".png");
+document.querySelector(".img2").setAttribute("src", "images/dice" + num2 + ".png");
+
+if (num1 > num2) {
+  document.querySelector("h1").textContent = "Player 1  wins, congrats.";
+} else if (num2 > num1) {
+  document.querySelector("h1").textContent = "Player 2 wins, congrats";
+} else {
+  document.querySelector("h1").textContent = "Draw, repeat, no sweat no fun";
+}
